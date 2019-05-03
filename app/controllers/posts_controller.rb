bfa0d5fbad2944611dfcc27734501d0ab301d01a
senzyuu <arozs08070@yahoo.co.jp>
2019-05-03 09:11:00 +0900

    編集機能完成｡

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -10,16 +12,19 @@ class PostsController < ApplicationController
   end
 
   def create
-    # byebug
     @post = Post.new(post_params)
     @post.save
     redirect_to @post
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
   end
 
   def destroy
